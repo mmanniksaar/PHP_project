b7f9beb4e4a648ca93c02155e00a35c422be8495
Marek Männiksaar <m.manniksaar@gmail.com>
2024-03-07 16:23:36 +0200

    azure deployment

---
+++ contact.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<header>
+    <?php include 'navbar.html'; ?>
+</header>
+
+<?php
+echo '
+
+<body>
+
+
+  <section class="vh-100 gradient-custom">
+    <div class="container py-4 h-100">
+      <div class="row justify-content-center align-items-center h-100">
+        <div class="col-12 col-lg-9 col-xl-7">
+          <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
+            <div class="card-body p-4 p-md-5">
+              <h1>Page Contact </h1>
+              <h3>Under development</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(41, 25, 25, 0.05);">
+      Copyright 2024
+
+    </div>
+  </section>
+
+</body>
+</html>'
+?>
